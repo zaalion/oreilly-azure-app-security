be742f46cabd61cbb78eb73654ebba98ec2f33ff
Reza Salehi <zaalion@users.noreply.github.com>
2020-02-07 09:10:32 -0500

    Project update

--- MSI/DemoFiles/GreetingO1/GreetingO1/Function1.cs
+++ MSI/DemoFiles/GreetingO1/GreetingO1/Function1.cs
@@ -1,25 +1,27 @@
+using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Microsoft.Azure.KeyVault;
-using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Azure.Services.AppAuthentication;
 
-namespace msi_before
+namespace GreetingO1
 {
     public static class Function1
     {
-        [FunctionName("GetSecretFromKV")]
-        public static  IActionResult Run(
+        [FunctionName("Function1")]
+        public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessToken));
 
-            var secretUrl = "https://kv-msi-01.vault.azure.net/secrets/myname/56c2905096f14c689d928da072139c72";
+            var secretUrl = "https://kvo1.vault.azure.net/secrets/myname/93d6f494fac6421bbf79a7c2fb1982ef";
             var secret = kv.GetSecretAsync(secretUrl).Result;
             var myName = secret.Value;
 
@@ -30,20 +32,20 @@ namespace msi_before
 
         private static async Task<string> GetAccessToken(string authority, string resource, string scope)
         {
-            //var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            //string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net");
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net");
 
-            //return accessToken;
+            return accessToken;
 
 
-            var clientId = "2c55e27d-ae31-461c-b5fc-ccd466c19e91";
-            var clientSecret = "2T:ynhBX_gR/DBL1Q66n.=clN4u*[0]j";
+            //var clientId = "e9c47b03-7788-4cf3-beaf-bf74bcc2a904";
+            //var clientSecret = "d:cQVwxNlYTMpUVj2kHN@mu@.vCCD555";
 
-            var authenticationContext = new AuthenticationContext(authority);
-            var cCreds = new ClientCredential(clientId, clientSecret);
-            AuthenticationResult result = await authenticationContext.AcquireTokenAsync(resource, cCreds);
+            //var authenticationContext = new AuthenticationContext(authority);
+            //var cCreds = new ClientCredential(clientId, clientSecret);
+            //AuthenticationResult result = await authenticationContext.AcquireTokenAsync(resource, cCreds);
 
-            return result.AccessToken;
+            //return result.AccessToken;
         }
     }
 }
