d03339beb2af62fe9a4c0b9a054d89e7e69a4353
Reza Salehi <zaalion@users.noreply.github.com>
2020-02-12 10:38:49 -0500

    Reset the code to initial stage

--- MSI/DemoFiles/GreetingO1/GreetingO1/Function1.cs
+++ MSI/DemoFiles/GreetingO1/GreetingO1/Function1.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 
 namespace GreetingO1
 {
@@ -19,11 +20,14 @@ namespace GreetingO1
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessToken));
 
+            // creating the Key Vault client
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(GetAccessToken));
             var secretUrl = "https://kvo1.vault.azure.net/secrets/myname/93d6f494fac6421bbf79a7c2fb1982ef";
             var secret = kv.GetSecretAsync(secretUrl).Result;
             var myName = secret.Value;
+
+            // var myName = "John";
 
             return myName != null
                 ? (ActionResult)new OkObjectResult($"Hello, {myName}")
@@ -32,20 +36,20 @@ namespace GreetingO1
 
         private static async Task<string> GetAccessToken(string authority, string resource, string scope)
         {
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net");
 
-            return accessToken;
+            //// Getting access the Key Vault access token using MSI (Managed Identities)
+            //var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            //string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net");
+            //return accessToken;
 
 
-            //var clientId = "e9c47b03-7788-4cf3-beaf-bf74bcc2a904";
-            //var clientSecret = "d:cQVwxNlYTMpUVj2kHN@mu@.vCCD555";
-
-            //var authenticationContext = new AuthenticationContext(authority);
-            //var cCreds = new ClientCredential(clientId, clientSecret);
-            //AuthenticationResult result = await authenticationContext.AcquireTokenAsync(resource, cCreds);
-
-            //return result.AccessToken;
+            // Getting access token from Azure Active Directory
+            var clientId = "";
+            var clientSecret = "";
+            var authenticationContext = new AuthenticationContext(authority);
+            var cCreds = new ClientCredential(clientId, clientSecret);
+            AuthenticationResult result = await authenticationContext.AcquireTokenAsync(resource, cCreds);
+            return result.AccessToken;
         }
     }
 }
